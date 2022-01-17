b648599539494923504efccd49df9170eac24679
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-01-17 07:28:26 +0530

    Add files via upload

---
+++ BINARY_SEARC/BINARY_SEARCH_COUNT ELEMENT.cpp
@@ -0,0 +1,66 @@
+// BINARY_SEARCH_COUNT ELEMENT.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include<iostream>
+using namespace std;
+void display(int array[], int size)
+{
+	cout << "arrray =  " << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << " ";
+		cout << array[i];
+	}
+}
+
+int count_element(int array[], int size)
+{
+	int low = 0, high = 0,count=0, mid, element;
+	high = size - 1;
+	
+	cout << " Enter an element present in array :  ";
+	cin >> element;
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		if (array[mid] == element)
+		{
+		cout << " " << element << " founded " << endl;
+		cout << " No of elements less than element is  " << mid ;
+		return mid;
+		}
+
+
+		else if  (array[mid] < element)
+		{
+			low = mid + 1;
+			high = size - 1;
+			
+		}
+		else
+		{
+			high = mid - 1;
+			
+		}
+
+		
+		
+	}
+	
+	return -1;
+}
+int main()
+{
+	int* array,size;
+	cout << "Enter an size : ";
+	cin >> size;
+	array = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		cout << "array[" << i << "] =  ";
+		cin >> array[i];
+	}
+	cout << endl;
+	display(array, size);
+	count_element(array, size);
+	return 0;
+
+}
