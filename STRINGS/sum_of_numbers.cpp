5b7c0b2678bf92da904ad05a2b91105bc87f355b
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-03-16 10:26:30 +0530

    Create sum_of_numbers.cpp

---
+++ STRINGS/sum_of_numbers.cpp
@@ -0,0 +1,18 @@
+ int findSum(string str)
+    {
+    	
+    	int sum=0;
+    	int num=0;
+    	for(int i=0;i<str.length();i++)
+    	{
+    	    if(str[i]>='0' && str[i]<='9')
+    	    {
+    	              num=num*10+str[i]-'0';
+    	    }
+    	    else
+    	    {
+    	        sum=sum+num;
+    	        num=0;
+    	    }
+    	}
+    	return sum+num;
