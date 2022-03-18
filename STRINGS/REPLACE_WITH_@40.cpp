7dac5dc70be569e678cb299289fe3458ba10edbc
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-03-18 10:19:44 +0530

    Add files via upload

---
+++ STRINGS/REPLACE_WITH_@40.cpp
@@ -0,0 +1,44 @@
+// REPLACE_WITH_@40.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+string orignal(string s)
+{
+    string temp = "";
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else
+        {
+            temp.push_back(s[i]);
+        }
+    }
+    
+    return temp;
+}
+
+int main()
+{
+    string s = "hello tejas how are you";
+    string s1=orignal(s);
+    cout << s1;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
