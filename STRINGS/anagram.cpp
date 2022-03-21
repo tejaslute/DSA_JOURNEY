0968604c65190ddb31ff639382961fe2ea9d2fd2
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-03-21 19:28:37 +0530

    Create anagram.cpp

---
+++ STRINGS/anagram.cpp
@@ -0,0 +1,23 @@
+/* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true */
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        if(s==t)
+        {
+   return true ;
+        }
+        return false;
+    }
+};
