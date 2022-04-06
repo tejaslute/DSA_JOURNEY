877a7035d8f04e0b8f0629227877be4995a8f2b3
Tejas <tejaslute24@gmail.com>
2022-04-06 08:34:23 +0530

    new'

---
+++ MATRIX/SEARCH_MATRIX_II.cpp
@@ -0,0 +1,35 @@
+// SEARCH_MATRIX_II.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int rowIndex = 0;
+        int colIndex = col - 1;
+
+        while (rowIndex < row && colIndex >= 0)
+        {
+            int element = matrix[rowIndex][colIndex];
+
+
+            if (element == target)
+            {
+                return 1;
+            }
+            else if (element < target)
+            {
+                rowIndex++;
+            }
+            else
+            {
+                colIndex--;
+            }
+        }
+        return 0;
+
+    }
+};
