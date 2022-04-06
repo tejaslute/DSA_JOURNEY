b3ae21aaba147b5c2e5bdc4968f6d8dae1ac29a7
Tejas <tejaslute24@gmail.com>
2022-04-06 08:54:38 +0530

    new

---
+++ MATRIX/COUNT_ZERO_IN_SORTED.cpp
@@ -0,0 +1,21 @@
+// COUNT_ZERO_IN_SORTED.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+int countZeros(vector<vector<int>>matrix)
+{
+	int row = matrix.size();
+	int col = matrix[0].size();
+
+	int count = 0;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < matrix[i].size(); j++)
+		{
+			if (matrix[i][j] == 0)
+			{
+				count++;
+			}
+		}
+	}
+	return count;
