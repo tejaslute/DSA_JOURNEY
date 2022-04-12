98bb1c5724cc5871723fafad05c34c26e2f75560
Tejas <tejaslute24@gmail.com>
2022-04-12 10:35:56 +0530

    new

---
+++ RECURSION/FIBONACCI_NUMBER.cpp
@@ -0,0 +1,19 @@
+// FIBONACCI_NUMBER.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0)
+        {
+            return 0;
+        }
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        int ans = fib(n - 1) + fib(n - 2);
+        return ans;
+
+    }
+};
