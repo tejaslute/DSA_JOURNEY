cd85246d9fe05974f341afc8213091ae592d2f15
{tejaslute} <{tejaslute24@gmail.com}>
2022-05-02 17:06:29 +0530

    new

---
+++ LINKED_LIST/CREATING_LINKED_LIST.cpp
@@ -0,0 +1,43 @@
+// CREATING_LINKED_LIST.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node* next;
+    void printlink(node* n)
+    {
+        while (n->data != NULL)
+        {
+            cout << "1st element " << n->data << endl;;
+            n = n->next;
+        }
+    }
+};
+int main()
+{
+   // creating an 3 nodes 
+    node* head = new node();
+    node* second = new node();
+    node* third = new node();
+    // assigning an value to it 
+
+    // first node 
+    head->data = 2;
+    head->next = second;
+    // second node
+    second->data = 3;
+    second->next = third;
+    // third node 
+    third->data = 4;
+    third->next = NULL;
+    // creation of object 
+    node n;
+    // traversing an linked list 
+
+    n.printlink(head);
+    return 0;
+}
