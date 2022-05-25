a0b100fb4cecc2471b3cc36cde48529f93f77aaa
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-05-25 20:18:08 +0530

    Update EUICLID_ALGORITHMS.cpp

--- ALGORITHMS/EUICLID_ALGORITHMS.cpp
+++ ALGORITHMS/EUICLID_ALGORITHMS.cpp
@@ -1,4 +1,4 @@
-// EUICLID_ALGORITHMS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// EUICLID_LGORITHMS.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
