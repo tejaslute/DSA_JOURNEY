75abfa5ffd27bc5652b018e843c162ef206c73e2
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-05-30 16:26:28 +0530

    Add files via upload

---
+++ ARRAY/FIRST_REPETING_ELEMENT.cpp
@@ -0,0 +1,17 @@
+int firstRepeated(int arr[], int n) {
+    // code hereint firstRepeated(int *arr, int n) {
+    unordered_map<int, int>mp;
+    for (int i = 0; i < n; i++)
+    {
+        mp[arr[i]]++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (mp[arr[i]] > 1)
+        {
+            return i + 1;
+        }
+    }
+    return -1;
+
+}
