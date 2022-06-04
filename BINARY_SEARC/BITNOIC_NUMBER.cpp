1a9590819a0217b00a688a716b3a32b80dbe344e
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-06-04 10:12:20 +0530

    Add files via upload

---
+++ BINARY_SEARC/BITNOIC_NUMBER.cpp
@@ -0,0 +1,31 @@
+//Given an array arr of n elements which is first increasingand then may be decreasing, find the maximum element in the array.
+//Note: If the array is increasing then just print then last element will be the maximum value.
+//
+//Example 1 :
+//
+//	Input :
+//	n = 9
+//	arr[] = { 1,15,25,45,42,21,17,12,11 }
+//Output: 45
+//Explanation : Maximum element is 45.
+
+
+int findMaximum(int arr[], int n) {
+	int start = 0;
+	int end = n - 1;
+	int mid = (start + end) / 2;
+	while (start < end)
+	{
+
+		if (arr[mid] < arr[mid + 1])
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid;
+		}
+		mid = (start + end) / 2;
+	}
+	return arr[start];
+}
