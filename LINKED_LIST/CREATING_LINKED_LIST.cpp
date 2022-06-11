d8491dcc37335df760b3a7bb43e9f1a364e336ea
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-06-11 22:01:21 +0530

    Update CREATING_LINKED_LIST.cpp

--- LINKED_LIST/CREATING_LINKED_LIST.cpp
+++ LINKED_LIST/CREATING_LINKED_LIST.cpp
@@ -1,4 +1,4 @@
-// CREATING_LINKED_LIST.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// CREATING_LINKED_LIST.cpp : This file contains the 'main' function. Prog ram execution begins and ends there.
 //
 
 #include <iostream>
