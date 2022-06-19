cf8021744dc47bb41e7459dfd3876e55d7aa5eeb
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-06-19 09:09:08 +0530

    Add files via upload

---
+++ LINKED_LIST/IMPLEMENT_STACK_USING_LINKDED_LIST.cpp
@@ -0,0 +1,20 @@
+void MyStack::push(int x)
+{
+    StackNode* temp = new StackNode(x);
+    temp->next = top;
+    top = temp;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack::pop()
+{
+    // Your Code
+    if (top == NULL)
+        return -1;
+
+    int x = top->data;
+    StackNode* del = top;
+    top = top->next;
+    delete del;
+    return x;
+}
