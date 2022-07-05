988aa0755ed085a1e445d29cd1fdb1806bcf3f12
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-07-05 07:26:13 +0530

    Add files via upload

---
+++ LINKED_LIST/MERGE_TWO_SORTED_LINK_LIST.cpp
@@ -0,0 +1,60 @@
+/*21. Merge Two Sorted Lists
+Easy
+
+13130
+
+1192
+
+Add to List
+
+Share
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list.The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+
+
+Example 1:
+
+
+Input: list1 = [1, 2, 4], list2 = [1, 3, 4]
+Output : [1, 1, 2, 3, 4, 4]
+Example 2 :
+
+	Input : list1 = [], list2 = []
+	Output : []
+	Example 3 :
+
+	Input : list1 = [], list2 = [0]
+	Output : [0]
+	*/
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy, * temp;
+        dummy = new ListNode();
+        temp = dummy;
+
+        //when both list1 and list2 isn't empty
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+
+        // we reached at the end of one of the list
+        if (list1) temp->next = list1;
+        if (list2) temp->next = list2;
+
+        return dummy->next;
+    }
+};
