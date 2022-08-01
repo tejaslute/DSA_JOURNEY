aa8bc99328a2e23614d25f5e2099b88a81481c04
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-08-01 07:30:15 +0530

    Add files via upload
    Implemented an  array which is able to perform an two stack opeartion . We can use single array for two stack . Perform an array operations of Stack1 from left to right and for stack 2 perform from right to left .

---
+++ STACK/TWO_STACK.cpp
@@ -0,0 +1,81 @@
+// TWO_STACK.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+class TwoStack {
+    int* arr;
+    int top1;
+    int top2;
+    int size;
+public:
+
+    // Initialize TwoStack.
+    TwoStack(int s) {
+        this->size = s;
+        top1 = -1;
+        top2 = s;
+        arr = new int[s];
+    }
+
+    // Push in stack 1.
+    void push1(int num) {
+        //atleast a empty space present
+        if (top2 - top1 > 1) {
+            top1++;
+            arr[top1] = num;
+        }
+
+    }
+
+    // Push in stack 2.
+    void push2(int num) {
+        if (top2 - top1 > 1) {
+            top2--;
+            arr[top2] = num;
+        }
+
+    }
+
+    // Pop from stack 1 and return popped element.
+    int pop1() {
+        if (top1 >= 0) {
+            int ans = arr[top1];
+            top1--;
+            return ans;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    // Pop from stack 2 and return popped element.
+    int pop2() {
+        if (top2 < size) {
+            int ans = arr[top2];
+            top2++;
+            return ans;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
