e045213db6e714ab738ee3312aea237f0d644ddc
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-08-10 08:36:05 +0530

    Update CREATING_TREE_USING_RECURSION.cpp

--- TREES/CREATING_TREE_USING_RECURSION.cpp
+++ TREES/CREATING_TREE_USING_RECURSION.cpp
@@ -38,22 +38,34 @@ public:
     {
         queue<Node*>q;
         q.push(root);
+        q.push(NULL);
         while (!q.empty())
         {
             Node* temp = q.front();
-            cout << temp-> data << " ";
+            
             q.pop();
-
-            if (temp->left)
+            if (temp == NULL)
             {
-                q.push(temp->left);
-
+                cout << endl;
+                if (!q.empty())
+                {
+                    q.push(NULL);
+                }
             }
-
-            if (temp->right)
+            else
             {
-                q.push(temp->right);
+                cout << temp->data << " ";
+                if (temp->left)
+                {
+                    q.push(temp->left);
 
+                }
+
+                if (temp->right)
+                {
+                    q.push(temp->right);
+
+                }
             }
         }
 
