587fcfde8db59db26ea124e2d8d5c95c53c7dbdc
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-08-15 00:17:36 +0530

    Update BITNOIC_NUMBER.cpp

--- BINARY_SEARC/BITNOIC_NUMBER.cpp
+++ BINARY_SEARC/BITNOIC_NUMBER.cpp
@@ -1,5 +1,5 @@
 //Given an array arr of n elements which is first increasingand then may be decreasing, find the maximum element in the array.
-//Note: If the array is increasing then just print then last element will be the maximum value.
+//Note: If the array is increvasing then just print then last element will be the maximum value.
 // 
 //Example 1 :
 //
