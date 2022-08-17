b7ec4ebe14d4073a3e42cbdd0af3bc7801aef7db
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-08-17 08:45:42 +0530

    Create reverse_first_k_elements.cpp

---
+++ QUEUE/reverse_first_k_elements.cpp
@@ -0,0 +1,28 @@
+//Reverse First K elements of Queue
+
+queue<int> modifyQueue(queue<int> q, int k) {
+    stack<int>s;
+    for(int i=0;i<k;i++)
+    {
+        int ans=q.front();
+        q.pop();
+        s.push(ans);
+    }
+    
+    while(!s.empty())
+    {
+        int ans=s.top();
+        s.pop();
+        q.push(ans);
+    }
+    
+    int t=q.size()-k;
+    while(t--)
+    {
+        int ans=q.front();
+        q.pop();
+        q.push(ans);
+    }
+    return q;
+    
+}
