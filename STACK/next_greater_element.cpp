f1463bb52fb5a83d354992f9086f8ae5f38cac11
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-08-19 09:58:16 +0530

    Create next_greater_element.cpp

---
+++ STACK/next_greater_element.cpp
@@ -0,0 +1,25 @@
+vector<long long> nextLargerElement(vector<long long> arr, int n){
+       vector<long long>ans;
+       stack<long long>s;
+       for(int i=n-1;i>=0;i--){  // loop to traverse array from backside 
+           if(s.size()==0){
+               ans.push_back(-1);
+           }
+           else if(s.size()>0 && s.top()>arr[i]){ // push an elemt if s.top()>arr[i];
+               ans.push_back(s.top());
+           }
+           else if(s.size()>0 && s.top()<arr[i]){  // do pop operation till top()>arr[i]
+               while(s.size()>0 && s.top()<arr[i]){
+                   s.pop();
+               }
+               if(s.size()==0){ // if no elemys remains then push -1 ;
+               ans.push_back(-1);
+           }
+           else{
+               ans.push_back(s.top());
+           }
+           }
+           s.push(arr[i]);
+       }
+       reverse(ans.begin(),ans.end()); // reverse an ans 
+       return ans;
