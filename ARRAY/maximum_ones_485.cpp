23c74b8f5deb3357676ba506061da001e3cd5dd9
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2023-08-03 09:16:02 +0530

    Create maximum_ones_485.cpp

---
+++ ARRAY/maximum_ones_485.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+
+     int count=0;
+     int max_count=0;
+     for(int i=0;i<nums.size();i++)
+     {
+         if(nums[i]==1)
+         {
+             count++;
+         }
+         else {
+             if(count>max_count)
+             {
+                 max_count=count;
+             }
+             count=0;
+         }
+
+         
+     }   
+     if(count>max_count)
+         {
+             return count;
+         }
+     return max_count;
+    }
+};
