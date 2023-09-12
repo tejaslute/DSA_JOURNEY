eb91fc0ccbd299223cdc9c1029c845e8d555bcdd
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2023-09-12 12:14:05 +0530

    Create number_even_odd.cpp

---
+++ BIT MANIPLUATION/number_even_odd.cpp
@@ -0,0 +1,21 @@
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int n=11;
+    
+    if(n&1==0)
+    {
+        cout<<"NUmber is even ";
+        
+    }
+    else {
+        cout<<"Number is odd ";
+    }
+    return 0;
+    
+    
+    
+}
