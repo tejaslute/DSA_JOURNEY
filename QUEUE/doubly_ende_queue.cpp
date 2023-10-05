17621fd85a820cacd462cf617b4ef1b6b47c7d3e
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2023-10-05 11:16:48 +0530

    Create doubly_ende_queue.cpp

---
+++ QUEUE/doubly_ende_queue.cpp
@@ -0,0 +1,63 @@
+class Deque
+{	
+    int *arr;
+    int front;
+    int rear;
+    int size;
+public:
+    // Initialize your data structure.
+    Deque(int n)
+    {
+        size = n;
+        arr = new int[n];
+        front = -1;
+        rear = -1;
+    }
+
+    // Pushes 'X' in the front of the deque. Returns true if it gets pushed into the deque, and false otherwise.
+    bool pushFront(int x)
+    {
+        //check full or not
+        if( isFull() ) {
+            return false;
+        }
+        else if(isEmpty()) {
+            front  = rear = 0;
+        }
+        else if(front == 0 && rear != size-1) {
+            front = size-1;
+        } 
+        else
+        {
+            front--;
+        }
+        arr[front] = x;
+        return true;
+    }
+
+    // Pushes 'X' in the back of the deque. Returns true if it gets pushed into the deque, and false otherwise.
+    bool pushRear(int x)
+    {
+        if( isFull() ) {
+            return false;
+        }
+        else if(isEmpty()) {
+            front  = rear = 0;
+        }
+        else if(rear == size-1 && front != 0) {
+            rear = 0;
+        } 
+        else
+        {
+             rear++;
+        }
+        arr[rear] = x;
+        return true;
+    }
+
+    // Pops an element from the front of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
+    int popFront()
+    {
+        if(isEmpty()){//to check queue is empty
+            //cout << "Queue is Empty " << endl;
+            return -1;
