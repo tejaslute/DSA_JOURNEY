1cac1715781b2c61b3e9151a53e4f290e9b8d6b2
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2023-11-22 12:24:54 +0530

    Create min_depth.cpp

---
+++ TREES/min_depth.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        
+        if(root==NULL)
+        {
+            return 0;
+        }
+
+        queue<TreeNode*>q;
+        int depth=1;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            int size=q.size();
+            while(size--)
+            {
+                TreeNode*temp=q.front();
+                q.pop();
+
+                if(temp->left==NULL && temp->right==NULL)
+                {
+                    return depth;
+                }
+                if(temp->left){
+                    q.push(temp->left);
+                }
+
+                if(temp->right){
+                    q.push(temp->right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
